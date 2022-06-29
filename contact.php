4308d9ca5b2bd208c0e3926259ad2098c38d892f
Troy Allen <jake.allen@me.com>
2022-06-29 18:23:31 -0400

    update hours

--- contact.php
+++ contact.php
@@ -24,13 +24,13 @@ include_once "header.php";
             </div>
             <div class="column">
                 <p>
-                    Monday 11:00 am - 6:30 pm<br>
+                    Monday 11:00 am - 7 pm<br>
                     Tuesday: Closed<br>
                     Wednesday: Closed<br>
-                    Thursday 11:00 am - 6:30 pm<br>
-                    Friday 11:00 am - 6:30 pm<br>
-                    Saturday 11:00 am - 6:30 pm<br>
-                    Sunday 11:00 am - 6:#0 pm<br>
+                    Thursday 11:00 am - 7 pm<br>
+                    Friday 11:00 am - 7 pm<br>
+                    Saturday 11:00 am - 7 pm<br>
+                    Sunday 11:00 am - 7 pm<br>
                 </p>
             </div>
         </div>
