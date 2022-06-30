dfd265f54109533045ffdae30549b4c057b25ad9
Troy Allen <jake.allen@me.com>
2022-06-29 21:31:52 -0400

    new images

--- gallery.php
+++ gallery.php
@@ -6,21 +6,18 @@ include_once "header.php";
         <div class="splide" role="group" aria-label="Splide Basic HTML Example">
             <div class="splide__track">
                 <ul class="splide__list">
-                    <li class="splide__slide"><img src="assets/images/gallery/gall1.jpg" alt=""></li>
+                    <li class="splide__slide"><img src="assets/images/newImages/couple.jpg" alt=""></li>
                     <li class="splide__slide"><img
                             src="assets/images/gallery/104497136_140630014327657_4070899253320356740_n_1618957172 Medium.jpeg"
                             alt="road sign"></li>
-                    <li class="splide__slide"><img
-                            src="assets/images/gallery/106929045_143795257344466_792293067530907464_n_1618957172 Medium.jpeg"
-                            alt="sign with flags"></li>
+                    <li class="splide__slide"><img src="assets/images/newImages/basket.jpg"
+                            alt="Delicious Basket of Fried Goodness"></li>
                     <li class="splide__slide"><img
                             src="assets/images/gallery/116643617_153679479689377_9104356878828305248_n_1618957172 Medium.jpeg"
                             alt="parking lot"></li>
-                    <li class="splide__slide"><img
-                            src="assets/images/gallery/118919038_167050468352278_1051010907117215228_n_1618957172 Medium.jpeg"
-                            alt="cindys logo"></li>
-                    <li class="splide__slide"><img src="assets/images/gallery/file_1618957136 Medium.jpeg"
-                            alt="fried clams"></li>
+                    <li class="splide__slide"><img src="assets/images/newImages/tacoAndRoll.jpg"
+                            alt="Fish Taco and Roll"></li>
+                    <li class="splide__slide"><img src="assets/images/newImages/shrimpRoll.jpg" alt="Shrimp Roll"></li>
                     <li class="splide__slide"><img src="assets/images/gallery/file1_1618957137 Medium.jpeg"
                             alt="basket of fried shrimp"></li>
                     <li class="splide__slide"><img src="assets/images/gallery/file2_1618957137 Medium.jpeg"
